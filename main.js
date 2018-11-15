cc88b64b375b4c052ecc7249a9a064a1f148483d
moueza <mouezapeter@gmail.com>
2018-11-15 13:13:21 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+google.discoverAPI({
+url: 'https://videointelligence.googleapis.com/$discovery/rest'
+}, function(serviceErr, videoService){
+const requestBody ={
+"auth":'video_api_key',
+"resource":{
+"inputUri":'your-gcs-video-filename',
+'features':['LABEL_DETECTION','SHOT_CHANGE_DETECTION']
+}
+}
+
+videoService.videos.annotate(requestBody, function(error,resp){
+const operationId = resp.name;
+google.discoverAPI({
+url : url,
+version: 'v1'
+key : apiKey
+},function(serviceErr, videoIntelligence){
+const reqBody={
+"auth":
+
+));
